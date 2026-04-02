ec1d63aebaf9ac89c577f8271f54f88b58b567e4
Reyan Ahmad <reyanansari7471@gmail.com>
2026-04-02 11:14:04 +0530

    Add files via upload

---
+++ Day 3/Demo.java
@@ -0,0 +1,16 @@
+// Stack & Heap Memory
+public class Demo {
+    public static void main(String[] args) {
+        int a = 10; // Stack memory
+        String str = "Hello"; // Heap memory
+
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of str: " + str);
+        // Assignment Operators
+        int b = 10; // Assignment operator
+        b += 5; // b = b + 5
+        System.out.println("Value of b: " + b);
+    }
+}
+
+
